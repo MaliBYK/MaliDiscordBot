e1376ecfdd3a779240d964043f59d461802e3129
heynaberuy <murathandorukk@gmail.com>
2021-04-26 16:39:31 +0300

    ban comm

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -1,5 +1,4 @@
 const { oneDescriptionEmbed } = require("@helpers/embeds.js");
-const { prefix } = process.env;
 
 module.exports = {
   commands: ["ban"],
@@ -8,23 +7,7 @@ module.exports = {
   cooldownMessage: "Wait {REMAINING} more execute this command again!",
   minArgs: 1,
   maxArgs: null,
-  argsMessage:
-    "Incorrect Arguments! Needed Args : <Member's @> (optional reason)",
-  dev: false,
-  devMessage: "You must be a developer to run this command !",
-  nsfw: false,
-  nsfwMessage: "You cannot run this command in SFW channels",
-  permissions: ["BAN_MEMBERS"],
-  permissionsMessage: "You do not have permission to use this command!",
-  botPermissions: ["BAN_MEMBERS"],
-  botPermissionsMessage:
-    "I cannot run this command without the 'Ban Members' permission!",
-  category: "Moderation",
-  locked: false,
-  lockedMessage: "This command is locked at the moment!",
-  hidden: false,
-  hidden2: false,
-  callback: ({ message, args, client, handler }) => {
+  callback: ( message, args, text ) => {
     const target = message.mentions.users.first();
     if (!target)
       return message.channel.send(
